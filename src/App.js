a9000de7dce48a2aacd2fceeee75d0cb50d57afa
EstebanHorn <Estebanhxrn@gmail.com>
2022-10-14 13:12:06 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import counterLogo from './assets/logo.png'
 import Button from './components/Button';
+import Counter from './components/Counter';
+import {useState} from 'react'
 
 function App() {
 
+  const [numClick, setNum] = useState(0);
+
+
   const clickEvent = () => {
-    console.log("click");
-  }
+    setNum(numClick + 1);
+  };
 
   const resetCounter = () =>{
-    console.log("reset");
-  }
+    setNum(0);
+  };
 
   return (
     <div className="App">
@@ -19,6 +24,7 @@ function App() {
         <img className='counter-logo' src={counterLogo} alt='counter Logo' />
       </div>
       <div className='main-container'>
+        <Counter numClick={numClick}/>
         <Button
         text="Click"
         isClick={true}
